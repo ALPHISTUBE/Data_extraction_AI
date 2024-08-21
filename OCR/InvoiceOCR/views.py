e1edf18b6f8df93d3c12351f15866ad712fa4cd2
ALPHISTUBE <65537475+ALPHISTUBE@users.noreply.github.com>
2024-08-21 15:56:49 +0600

    Invoice new format add

--- OCR/InvoiceOCR/views.py
+++ OCR/InvoiceOCR/views.py
@@ -40,7 +40,7 @@ def index(request):
             print(extracted_text)
 
             # Process extracted text
-            currectionData, date, totalA = formatTextFromReceipt(extracted_text)
+            currectionData, date, totalA = formatTableTextFromLine(extracted_text)
 
             return render(request, 'iocr/index.html', {
                 'form': form,
@@ -94,20 +94,21 @@ def currectSpelling(text):
     corrected_words = " ".join([spell.correction(word) if word not in spell else word for word in words])
     return corrected_words
 
-def formatTextFromReceipt(text):
+def formatTableTextFromLine(text):
     et = {}
     lines = text.splitlines()
     dates = []
+    mainDate = ""
     total_amount = 0.0
     indexl = 0
     for line in lines:
         line = line.lower().replace("\n", "").replace("_", " ")
         date = ""
         dt_ext = extract_dates(line)
-
         if len(dt_ext) > 0:
             indexl += 1
             date = dt_ext[0]
+            dates.append(date)
             line = line.replace(date, "").replace(",", "")
             data = re.findall(r"\d+\.\d+", line)
             float_data = [float(num) for num in data] if data else []
@@ -118,10 +119,139 @@ def formatTextFromReceipt(text):
             else:
                 et[f"{indexl}"] = [date, line, "NONE"]
 
-    if dates:
+    mainDate = dates[0]
+    algoWorked = CheckDateAndAmountRadio(et)
+    print(algoWorked)
+    total_amount = round(total_amount, 2)
+    if not algoWorked:
+        et, mainDate, total_amount = formatTextFrom3ColumeTable(text)
+
+    if mainDate != "":
+        return et, mainDate, total_amount
+    else:
+        return et, "No Date Found", total_amount
+
+
+tableHeaderKey = [
+    "date",
+    "description",
+    "amount"
+]
+
+usa_st_code = [
+    'AK', 'AL', 'AR', 'AS', 'AZ', 'CA', 'CO', 'CT', 'DC', 'DE',
+    'FL', 'FM', 'GA', 'GU', 'HI', 'IA', 'ID', 'IL', 'IN', 'KS',
+    'KY', 'LA', 'MA', 'MD', 'ME', 'MH', 'MI', 'MN', 'MO', 'MP',
+    'MS', 'MT', 'NC', 'ND', 'NE', 'NH', 'NJ', 'NM', 'NV', 'NY',
+    'OH', 'OK', 'OR', 'PA', 'PR', 'PW', 'RI', 'SC', 'SD', 'TN',
+    'TX', 'UT', 'VA', 'VI', 'VT', 'WA', 'WI', 'WV', 'WY', "WEB"
+]
+
+def formatTextFrom3ColumeTable(text):
+    et = {}
+    dates = []
+    descriptions = []
+    amounts = []
+    lines = text.splitlines()
+    total_amount = 0.0
+
+    indexl = 0
+    headerIndex = 0
+    headerText = ""
+    desI = 0
+    amoI = 0
+    for line in lines:
+        line = str(line.lower())
+
+        if headerIndex <= 2 and line.find(tableHeaderKey[headerIndex]) != -1:
+            headerText = tableHeaderKey[headerIndex]
+            headerIndex += 1
+        
+        
+        #Date
+        dt_ext = extract_dates(line)
+        if headerText == "date" and line != "":
+            if len(dt_ext) > 0:
+                dates.append(dt_ext[0])            
+
+        #Description
+        if headerText == "description" and line != "":
+
+            codes = line.split(" ")
+            hasCode = codes[len(codes) - 1].upper() in usa_st_code
+            print(hasCode)
+            if len(dt_ext) == 0 and desI != 0 and hasCode:
+                descriptions.append(line)
+            if len(dt_ext) == 0 and desI != 0 and not hasCode:
+                text = descriptions[len(descriptions) - 1]
+                descriptions.append(f"{text}\n{line}")
+            desI = 1
+
+        #Amount
+        if headerText == "amount" and line != "":
+            line = line.replace(",", "")
+            
+            if amoI != 0:
+                # data = re.findall(r"\d+\.\d+", line)
+                data = getAmount(line)
+                float_data = [float(num) for num in data] if data else []
+                if float_data:
+                    total_amount += float_data[0]
+                    line = line.replace(data[0], "")
+                    amounts.append(data[0])
+                else:
+                    amounts.append("NONE")
+            amoI = 1
+
+        dt_ext = extract_dates(line)
+
+    print(len(descriptions))
+    for date in dates:
+        et[f"{indexl}"] = [date, descriptions[indexl], amounts[indexl]]
+        indexl += 1
+
+    total_amount = round(total_amount, 2)
+    if dates[0] != "":
         return et, dates[0], total_amount
     else:
         return et, "No Date Found", total_amount
+
+def CheckDateAndAmountRadio(formattedData : dict):
+    
+    totalTranc = len(formattedData)
+    dataFound = 0
+    
+    for data in formattedData.values():
+        print(data)
+        if data[0] != "" and data[1] != "" and data[2] != "NONE":
+            dataFound += 1
+        else:
+            dataFound -+ 1
+
+    if dataFound <= totalTranc / 2:
+        return False
+    else:
+        return True
+
+def getAmount(line):
+    print(line)
+    data = re.findall(r"\d+\.\d+", line)
+    data1 = re.findall(r"\d+\.\d+\.\d+", line)
+    data2 = re.findall(r"\.\d+", line)
+
+    if data:
+        return data
+    elif data1:
+        text = data1[0][0:len(line) - 3]
+        data = re.findall(r"\d+\.\d+", text)
+        if data:
+            return data
+    elif data2:
+        text = "0" + data2[0]
+        data2[0] = text
+        return data2
+    else:
+        return ["NONE"]
 
 def extract_dates(text):
     date_patterns = [
